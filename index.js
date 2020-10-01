9cde9a64efbf0b3a3b4e099d05817a7f887d20fe
thomas <thomas.chalanson@sfr.fr>
2020-10-01 16:28:47 +0200

    give 4 colors to the names

---
+++ index.js
@@ -0,0 +1,8 @@
+let wilders = ["Aymeric", "Florian", "Jimmy", "Laura"];
+
+const chalk = require('chalk');
+ 
+console.log(chalk.blue(wilders[0]));
+console.log(chalk.red(wilders[1]));
+console.log(chalk.yellow(wilders[2]));
+console.log(chalk.magenta(wilders[3]));
